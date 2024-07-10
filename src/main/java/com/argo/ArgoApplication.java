05ed5570983443de5a53f7fe19f9ea2d8db74756
igyeong-yun <xxxlsu@naver.com>
2024-07-10 13:56:10 +0900

    init commit

---
+++ src/main/java/com/argo/ArgoApplication.java
@@ -0,0 +1,13 @@
+package com.argo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArgoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArgoApplication.class, args);
+    }
+
+}
